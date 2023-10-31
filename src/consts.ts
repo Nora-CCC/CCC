436d0b9b1531be683617ddac87d4275e3ba0bcf0
nicolas-trillo <nicolastrillo@icloud.com>
2023-10-31 15:46:55 -0400

    Switch to astro

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Cornwall Computer Club";
+export const SITE_TITLE_SHORT = "CCC";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const GITHUB = "https://github.com/Nora-CCC/CCC";
+export const XDOTCOM = "https://x.com";
